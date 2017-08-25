410c3dbbbda52a89beb686781365ced9f8823450
bobzz-zone <bobzz.zone@gmail.com>
2017-08-25 17:55:01 +0700

    add

---
+++ rsi/rsi/report/summary_penjualan/summary_penjualan.js
@@ -0,0 +1,5 @@
+
+frappe.query_reports["Account Payable Detail"] = {
+	"filters": [
+	]
+}
---
+++ rsi/rsi/report/summary_penjualan/summary_penjualan.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+import frappe
+def execute(filters=None):
+	data=[]
+	columns=[]
+	return data
