c50d99b5378a04032bf55b77031239e92df98fc9
Bobby <bobzz.zone@gmail.com>
2017-08-26 04:38:58 +0000

    report

---
+++ rsi/rsi/report/summary_retur_pembelian/summary_retur_pembelian.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, myme and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Summary Retur Pembelian"] = {
+	"filters": [
+
+	]
+}
---
+++ rsi/rsi/report/tanda_terima_nota_tagihan_salesman_dari_admin/tanda_terima_nota_tagihan_salesman_dari_admin.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, myme and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Tanda Terima Nota Tagihan Salesman dari Admin"] = {
+	"filters": [
+
+	]
+}
