cf040475588ba6749e61e85431c18af572a15067
bobzz-zone <bobzz.zone@gmail.com>
2017-08-26 14:14:07 +0700

    a

--- rsi/rsi/report/summary_retur_pembelian/summary_retur_pembelian.py
+++ rsi/rsi/report/summary_retur_pembelian/summary_retur_pembelian.py
@@ -8,12 +8,12 @@ def execute(filters=None):
 	columns, data = ["Tanggal:Date:150","Hari:Data:150","Faktur:Int:100","Jumlah:Currency:300"], []
 	where=""
 	if filters.get("warehouse"):
-		where=""" and warehouse="{}" """.format(filters.get("warehouse"))
+		where=""" and a.warehouse="{}" """.format(filters.get("warehouse"))
 	if filters.get("supplier"):
-		where=""" {} and supplier="{}" """.format(where ,filters.get("supplier"))
+		where=""" {} and a.supplier="{}" """.format(where ,filters.get("supplier"))
 	if filters.get("remark"):
-		where=""" {} and remarks like "%{}%" """.format(where,filters.get("remark"))
-	data = frappe.db.sql("""select posting_date,date_format(posting_date,"%W"),count(1) as "qty" , sum(base_grand_total) as "omset" 
+		where=""" {} and a.remarks like "%{}%" """.format(where,filters.get("remark"))
+	data = frappe.db.sql("""select a.posting_date,date_format(a.posting_date,"%W"),count(1) as "qty" , sum(a.base_grand_total) as "omset" 
 		from (select p.posting_date,p.base_grand_total,pi.warehouse,p.remarks,p.supplier,p.is_return,p.docstatus from `tabPurchase Invoice` p join `tabPurchase Invoice Item` pi on p.name=pi.parent group by name) 
-		where docstatus=1 and is_return=1 {} and (posting_date between "{}" and "{}") group by posting_date """.format(where,filters.get("from_date"),filters.get("to_date")),as_list=1)
+		where a.docstatus=1 and a.is_return=1 {} and (posting_date between "{}" and "{}") a group by a.posting_date """.format(where,filters.get("from_date"),filters.get("to_date")),as_list=1)
 	return columns, data
